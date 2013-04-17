d4f9df7492c5159b365e8530c5dc54af7ce9cc8b
Marin Usalj <mneorr@gmail.com>
2013-04-17 18:19:31 +0200

    Initial Commit

---
+++ Alcatraz/Alcatraz.h
@@ -0,0 +1,13 @@
+//
+//  Alcatraz.h
+//  Alcatraz
+//
+//  Created by Marin Usalj on 4/17/13.
+//  Copyright (c) 2013 mneorr.com. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface Alcatraz : NSObject
+
+@end
---
+++ Alcatraz/Alcatraz.m
@@ -0,0 +1,53 @@
+//
+//  Alcatraz.m
+//  Alcatraz
+//
+//  Created by Marin Usalj on 4/17/13.
+//  Copyright (c) 2013 mneorr.com. All rights reserved.
+//
+
+#import "Alcatraz.h"
+
+@implementation Alcatraz
+
+
++ (void)pluginDidLoad:(NSBundle *)plugin
+{
+    static id sharedPlugin = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedPlugin = [[self alloc] init];
+    });
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        // Create menu items, initialize UI, etc.
+
+        // Sample Menu Item:
+        NSMenuItem *viewMenuItem = [[NSApp mainMenu] itemWithTitle:@"File"];
+        if (viewMenuItem) {
+            [[viewMenuItem submenu] addItem:[NSMenuItem separatorItem]];
+            NSMenuItem *sample = [[[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""] autorelease];
+            [sample setTarget:self];
+            [[viewMenuItem submenu] addItem:sample];
+        }
+    }
+    return self;
+}
+
+// Sample Action, for menu item:
+- (void) doMenuAction
+{
+    NSAlert *alert = [NSAlert alertWithMessageText:@"Hello, World" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@""];
+    [alert runModal];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [super dealloc];
+}
+
+@end
