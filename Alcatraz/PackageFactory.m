e004a5aae8ad049eb282e23af0cb38df221fa0a8
Marin Usalj <mneorr@gmail.com>
2013-04-18 15:18:16 +0200

    license stuff

--- Alcatraz/PackageFactory.m
+++ Alcatraz/PackageFactory.m
@@ -1,5 +1,5 @@
-// Plugin.h
-//
+// PackageFactory.m
+// 
 // Copyright (c) 2013 mneorr.com
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
+// 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-//
+// 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,9 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-#import <Foundation/Foundation.h>
-#import "Package.h"
+#import "PackageFactory.h"
 
-@interface Plugin : Package
+@implementation PackageFactory
 
 @end
