357989b81e6843e0352d1e89218d7f0e64fe87c0
Marin Usalj <mneorr@gmail.com>
2013-05-11 20:56:42 +0200

    Loads bundle

--- AlcatrazTests/AlcatrazTests.m
+++ AlcatrazTests/AlcatrazTests.m
@@ -14,8 +14,33 @@ describe(@"Loading plugin", ^{
    
     it(@"creates 'Package Manager' menu item under Window submenu", ^{
        
-        [[@3 should] equal:@4];
         
+        
+        NSString *ALCATRAZ_PATH = [NSHomeDirectory() stringByAppendingPathComponent:@"Library/Application Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin"];
+        
+        
+        
+        NSBundle *pluginBundle = [NSBundle bundleWithPath:ALCATRAZ_PATH];
+        
+        NSLog(@"WAS IT LOADED? %@", @([pluginBundle isLoaded]));
+        
+        NSError *error = nil;
+        [pluginBundle loadAndReturnError:&error];
+        [[pluginBundle principalClass] performSelector:@selector(pluginDidLoad:) withObject:pluginBundle];
+        if (error)
+            NSLog(@"ZOMG ERRRO! %@", error);
+        
+        NSLog(@"WAS IT LOADED AFTER? %@", @([pluginBundle isLoaded]));
+        
+        
+        NSMenuItem *windowMenuItem = [[NSApp mainMenu] itemWithTitle:@"Window"];
+        
+        
+        NSMenuItem *alcatrazItem = [[windowMenuItem submenu] itemWithTitle:@"Package Manager"];
+        NSLog(@"APP: %@ , BUNDLE: %@ MENU: %@", [NSApplication sharedApplication] , [NSBundle mainBundle], [NSApp mainMenu]);
+        [alcatrazItem shouldNotBeNil];
+        
+        [[@([windowMenuItem.submenu indexOfItem:alcatrazItem]) should] equal:@([[windowMenuItem submenu] indexOfItemWithTitle:@"Organizer"] + 1)];
     });
     
 });
