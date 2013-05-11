fecdccbd10a34c07b158cdceef674d5f5e355b96
Marin Usalj <mneorr@gmail.com>
2013-05-11 20:02:57 +0200

    Add Kiwi

---
+++ AlcatrazTests/AlcatrazTests.m
@@ -0,0 +1,23 @@
+//
+//  AlcatrazTests.m
+//  AlcatrazTests
+//
+//  Created by Marin Usalj on 5/11/13.
+//  Copyright (c) 2013 mneorr.com. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(AlcatrazTests)
+
+describe(@"Loading plugin", ^{
+   
+    it(@"creates 'Package Manager' menu item under Window submenu", ^{
+       
+        [[@3 should] equal:@4];
+        
+    });
+    
+});
+
+SPEC_END
