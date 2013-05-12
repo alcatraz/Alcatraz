202e6e15fb781e2048313ae4974ecb48aa312665
Marin Usalj <mneorr@gmail.com>
2013-05-12 18:45:51 +0200

    tests are passing using xcodebuild. don't touch xctool

--- AlcatrazTests/AlcatrazTests.m
+++ AlcatrazTests/AlcatrazTests.m
@@ -7,41 +7,91 @@
 //
 
 #import "Kiwi.h"
+#import "Alcatraz.h"
+#import "ATZShell.h"
+
+static NSString *const ALCATRAZ_PATH = @"Library/Application Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin";
+
+NSMenu *createFakeMenu() {
+    NSMenu *fakeMenu = [[NSMenu alloc] initWithTitle:@"Alcatraz"];
+    NSMenuItem *windowMenu = [[NSMenuItem alloc] initWithTitle:@"Window" action:nil keyEquivalent:@""];
+    windowMenu.submenu = [[NSMenu alloc] initWithTitle:@"FakeSubmenu"];
+    [windowMenu.submenu addItemWithTitle:@"Organizer" action:nil keyEquivalent:@""];
+    
+    [fakeMenu addItem:windowMenu];
+    return fakeMenu;
+}
+
+void clickMenuItem() {
+    NSMenuItem *windowMenuItem = [[NSApp mainMenu] itemWithTitle:@"Window"];
+    NSMenuItem *alcatrazItem = [[windowMenuItem submenu] itemWithTitle:@"Package Manager"];
+    [alcatrazItem.target performSelector:alcatrazItem.action];
+}
 
 SPEC_BEGIN(AlcatrazTests)
 
-describe(@"Loading plugin", ^{
+describe(@"Alcatraz.m", ^{
    
-    it(@"creates 'Package Manager' menu item under Window submenu", ^{
-       
+    __block Alcatraz *alcatraz;
+    __block NSError *error = nil;
+    NSBundle *pluginBundle = [NSBundle bundleWithPath:[NSHomeDirectory() stringByAppendingPathComponent:ALCATRAZ_PATH]];
+    
+    beforeEach(^{
+        [NSApp stub:@selector(mainMenu) andReturn:createFakeMenu()];
         
-        
-        NSString *ALCATRAZ_PATH = [NSHomeDirectory() stringByAppendingPathComponent:@"Library/Application Support/Developer/Shared/Xcode/Plug-ins/Alcatraz.xcplugin"];
-        
-        
-        
-        NSBundle *pluginBundle = [NSBundle bundleWithPath:ALCATRAZ_PATH];
-        
-        NSLog(@"WAS IT LOADED? %@", @([pluginBundle isLoaded]));
-        
-        NSError *error = nil;
         [pluginBundle loadAndReturnError:&error];
         [[pluginBundle principalClass] performSelector:@selector(pluginDidLoad:) withObject:pluginBundle];
-        if (error)
-            NSLog(@"ZOMG ERRRO! %@", error);
         
-        NSLog(@"WAS IT LOADED AFTER? %@", @([pluginBundle isLoaded]));
-        
-        
-        NSMenuItem *windowMenuItem = [[NSApp mainMenu] itemWithTitle:@"Window"];
-        
-        
-        NSMenuItem *alcatrazItem = [[windowMenuItem submenu] itemWithTitle:@"Package Manager"];
-        NSLog(@"APP: %@ , BUNDLE: %@ MENU: %@", [NSApplication sharedApplication] , [NSBundle mainBundle], [NSApp mainMenu]);
-        [alcatrazItem shouldNotBeNil];
-        
-        [[@([windowMenuItem.submenu indexOfItem:alcatrazItem]) should] equal:@([[windowMenuItem submenu] indexOfItemWithTitle:@"Organizer"] + 1)];
+        if (error) NSLog(@"ZOMG ERRRO! %@", error);
+        alcatraz = [[[pluginBundle principalClass] alloc] performSelector:@selector(initWithBundle:) withObject:pluginBundle];
     });
+    
+    describe(@"loading plugin", ^{
+    
+        it(@"creates 'Package Manager' menu item under Window submenu", ^{
+            NSMenuItem *windowMenuItem = [[NSApp mainMenu] itemWithTitle:@"Window"];
+            NSMenuItem *alcatrazItem = [[windowMenuItem submenu] itemWithTitle:@"Package Manager"];
+            
+            [alcatrazItem shouldNotBeNil];
+            [[@([windowMenuItem.submenu indexOfItem:alcatrazItem]) should] equal:@([[windowMenuItem submenu] indexOfItemWithTitle:@"Organizer"] + 1)];
+        });
+        
+    });
+
+    describe(@"pressing the menu item", ^{
+        
+        context(@"command line tools are available", ^{
+            
+            it(@"loads alcatraz window and puts it in front", ^{
+                [[alcatraz should] receive:@selector(loadWindowAndPutInFront)];
+                clickMenuItem();
+            });
+            
+        });
+        
+        context(@"command line tools are not available", ^{
+            
+            KWMock *mockAlert = [NSAlert nullMock];
+            
+            beforeEach(^{
+                [NSTask stub:@selector(launchedTaskWithLaunchPath:arguments:) withBlock:^id(NSArray *params) {
+                    @throw [NSException exceptionWithName:@"HAI" reason:nil userInfo:nil];
+                }];
+                [NSAlert stub:@selector(alertWithMessageText:defaultButton:alternateButton:otherButton:informativeTextWithFormat:)
+                    andReturn:mockAlert];
+            });
+            
+            it(@"doesn't display the window", ^{
+                [[alcatraz shouldNot] receive:@selector(loadWindowAndPutInFront)];
+                clickMenuItem();
+            });
+            
+            it(@"alerts the user", ^{
+                [[mockAlert should] receive:@selector(runModal)];
+                clickMenuItem();
+            });
+        });
+    });    
     
 });
 
