ae0a70d134676e09f41971c984c060688692837b
Marin Usalj <mneorr@gmail.com>
2013-05-15 09:36:03 +0200

    updated Kiwi to 2.1 and used string matchers

--- AlcatrazTests/Packages/ATZPackageTests.m
+++ AlcatrazTests/Packages/ATZPackageTests.m
@@ -14,10 +14,10 @@ SPEC_BEGIN(ATZPackageTests)
 describe(@"parsing website", ^{
 
     __block ATZPackage *package;
-    __block NSString   *userName = @"userName";
-    __block NSString   *repoName = @"lifeRepo";
+    NSString *userName = @"userName";
+    NSString *repoName = @"lifeRepo";
 
-    beforeAll(^{
+    beforeEach(^{
         package = [[ATZPackage alloc] initWithDictionary:@{
             @"name"        : @"Life",
             @"description" : @"A short game of numbers"
@@ -26,18 +26,15 @@ describe(@"parsing website", ^{
 
     it(@"creates project page URL from raw github URL", ^{
         package.remotePath = [NSString stringWithFormat:@"http://raw.github.com/%@/%@/branch/file",userName,repoName];
-        NSString *website = [package website];
-        [[theValue([website rangeOfString:@"https://github.com"].location) should] equal:@0];
-        [[theValue([website rangeOfString:userName].location) shouldNot] equal:theValue(NSNotFound)];
-        [[theValue([website rangeOfString:repoName].location) shouldNot] equal:theValue(NSNotFound)];
-//        [[website should] startWithString:@"https://github.com"];
-//        [[website should] containString:userName];
-//        [[website should] containString:repoName];
+        
+        [[package.website should] startWithString:@"https://github.com"];
+        [[package.website should] containString:userName];
+        [[package.website should] containString:repoName];
     });
 
     it(@"preserves URL when not a raw github URL", ^{
         package.remotePath = @"http://git.example.com/life.git";
-        [[[package website] should] equal:package.remotePath];
+        [[package.website should] equal:package.remotePath];
     });
 });
 
