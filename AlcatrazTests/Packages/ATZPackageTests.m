3e37d805fc59ea5f8403553c2da8b4791596beba
Marin Usalj <mneorr@gmail.com>
2013-10-07 20:51:10 -0700

    removed compatibility specs

--- AlcatrazTests/Packages/ATZPackageTests.m
+++ AlcatrazTests/Packages/ATZPackageTests.m
@@ -92,43 +92,6 @@ describe(@"Package", ^{
         });
         
     });
-
-    describe(@"Xcode compatibility checking", ^{
-
-        __block Alcatraz *mockATZ;
-
-        beforeEach(^{
-            mockATZ = [Alcatraz mock];
-            [Alcatraz stub:@selector(sharedPlugin) andReturn:mockATZ];
-            [mockATZ stub:@selector(xcodeMajorVersion) andReturn:@"17"];
-            [mockATZ stub:@selector(xcodeMinorVersion) andReturn:@"5"];
-        });
-
-        it(@"assumes compatibility based on major version", ^{
-            ATZPackage *newPackage = [[ATZPackage alloc] initWithDictionary:@{ @"xcode_version": @"17" }];
-            [[theValue([newPackage isCompatibleWithXcode]) should] equal:theValue(YES)];
-        });
-
-        it(@"assumes compatibility based on major and minor version", ^{
-            ATZPackage *newPackage = [[ATZPackage alloc] initWithDictionary:@{ @"xcode_version": @"17.5" }];
-            [[theValue([newPackage isCompatibleWithXcode]) should] equal:theValue(YES)];
-        });
-
-        it(@"assumes compatibility when package version is not specified", ^{
-            ATZPackage *newPackage = [[ATZPackage alloc] initWithDictionary:@{}];
-            [[theValue([newPackage isCompatibleWithXcode]) should] equal:theValue(YES)];
-        });
-
-        it(@"rejects based on non-matching major version", ^{
-            ATZPackage *newPackage = [[ATZPackage alloc] initWithDictionary:@{ @"xcode_version": @"1" }];
-            [[theValue([newPackage isCompatibleWithXcode]) should] equal:theValue(NO)];
-        });
-
-        it(@"rejects based on non-matching minor version", ^{
-            ATZPackage *newPackage = [[ATZPackage alloc] initWithDictionary:@{ @"xcode_version": @"17.1" }];
-            [[theValue([newPackage isCompatibleWithXcode]) should] equal:theValue(NO)];
-        });
-    });
     
     describe(@"installing", ^{
         
