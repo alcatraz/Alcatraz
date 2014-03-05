4b5966dbfd0484329190e72d689d56896c171a09
Marin Usalj <mneorr@gmail.com>
2014-03-05 15:57:03 -0800

    ignore _site, update install instructions

--- _site/javascripts/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var components = window.location.toString().split("/");
-  if (components.length > 4) {
-    $("#"+components[3]).addClass("active");
-  } else {
-    $("#index").addClass("active");
-  }
-});
