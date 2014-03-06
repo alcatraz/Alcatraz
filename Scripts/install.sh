41b8fcf31885b51253935bef2c2c75faa2f36758
Marin Usalj <mneorr@gmail.com>
2014-03-05 16:06:15 -0800

    add post install message

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,3 +4,5 @@ PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins
 mkdir -p $PLUGINS_DIR
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
+echo "Alcatraz successfuly installed!!1!🍻   Please restart your Xcode."
+
