fa73de4e552e4c738a34c37e207e4aed6b45c17e
Marin Usalj <mneorr@gmail.com>
2014-03-07 09:58:33 -0800

    apply consistent  on variables

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 DOWNLOAD_URI=https://github.com/supermarin/Alcatraz/releases/download/1.0.1/Alcatraz.tar.gz
 PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
 
-mkdir -p "$PLUGINS_DIR"
+mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
 echo "Alcatraz successfuly installed!!1!🍻   Please restart your Xcode."
