7d9fa5542248f5ce8ee9fc9801f6967cfe0c5c41
Mark Schall <mark@detroitlabs.com>
2014-08-29 16:21:25 -0400

    correcting the copyright information

--- Alcatraz/Packages/ATZCodeSnippet.m
+++ Alcatraz/Packages/ATZCodeSnippet.m
@@ -1,6 +1,6 @@
 //  ATZSnippet.m
 //
-// Copyright (c) 2014 Mark Schall | Detroit Labs
+// Copyright (c) 2013 Marin Usalj | supermar.in
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
