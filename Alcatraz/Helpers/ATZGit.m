f2cca51da2c895dda5c10dabf64f59cb97f71cf6
John Holdsworth <github@johnholdsworth.com>
2015-03-12 17:09:24 +0000

    A one line change to solve a lot of problem installing plugins with nibs

--- Alcatraz/Helpers/ATZGit.m
+++ Alcatraz/Helpers/ATZGit.m
@@ -87,7 +87,8 @@
 }
 
 + (void)fetch:(NSString *)localPath completion:(void (^)(NSString *, NSError *))completion {
-    
+    [[NSFileManager sharedManager] removeItemAtPath:[localPath stringByAppendingPathComponent:@"build"] error:NULL];
+
     ATZShell *shell = [ATZShell new];
     [shell executeCommand:GIT withArguments:@[FETCH, ORIGIN] inWorkingDirectory:localPath
                completion:^(NSString *output, NSError *error) {
