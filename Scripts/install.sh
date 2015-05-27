ba11520d970238bd0d1f4c01f9fed67a89e2a415
Pablo Bendersky <pbendersky@quadiontech.com>
2015-05-27 13:55:06 -0300

    Changed installer to remove blacklisted plugins, so the user will be prompted to enable bundles when re-opening Xcode.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,5 +6,7 @@ PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins
 mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
+defaults delete com.apple.dt.Xcode.plist DVTPlugInManagerNonApplePlugIns-Xcode-6.3.2 2> /dev/null
+
 echo "Alcatraz successfully installed!!1!🍻   Please restart your Xcode."
 
