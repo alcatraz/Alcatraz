9de984cfb28697c9a1fd3656b412b6b340c3df1a
Delisa Mason <iskanamagus@gmail.com>
2015-06-08 18:23:30 -0700

    Update install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -6,5 +6,6 @@ PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins
 mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
+# the 1 is not a typo!
 echo "Alcatraz successfully installed!!1!🍻   Please restart your Xcode."
 
