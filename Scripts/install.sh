767f87d2624801ea572f685b533aa129e1657e2c
Guillaume Algis <guillaume.algis@gmail.com>
2015-07-05 18:36:15 +0200

    Display currently selected Xcode version in install script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -22,5 +22,5 @@ mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
 # the 1 is not a typo!
-echo "Alcatraz successfully installed!!1!🍻   Please restart your Xcode."
+echo "Alcatraz successfully installed!!1!🍻   Please restart your Xcode ($XCODE_VERSION)."
 
