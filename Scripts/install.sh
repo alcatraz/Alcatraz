c84f80a28671ef1f94408c2e537608993fd57a2c
Guillaume Algis <guillaume.algis@gmail.com>
2015-07-05 18:33:24 +0200

    Remove Alcatraz from the Xcode skipped plugins list at install

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,6 +4,19 @@ set -euo pipefail
 
 DOWNLOAD_URI=https://github.com/alcatraz/Alcatraz/releases/download/1.1.13/Alcatraz.tar.gz
 PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
+XCODE_VERSION="$(xcrun xcodebuild -version | head -n1 | awk '{ print $2 }')"
+PLIST_PLUGINS_KEY="DVTPlugInManagerNonApplePlugIns-Xcode-${XCODE_VERSION}"
+BUNDLE_ID="com.mneorr.Alcatraz"
+TMP_FILE="/tmp/${BUNDLE_ID}.xcode-defaults"
+
+# Remove Alcatraz from Xcode's skipped plugins list if needed
+defaults read -app Xcode "$PLIST_PLUGINS_KEY" > "$TMP_FILE"
+/usr/libexec/PlistBuddy -c "delete skipped:$BUNDLE_ID" "$TMP_FILE" > /dev/null 2>&1 && {
+	pgrep Xcode > /dev/null && { echo 'An instance of Xcode is currently running. Please close Xcode before installing Alcatraz.'; exit 1; }
+	defaults write -app Xcode "$PLIST_PLUGINS_KEY" "$(< $TMP_FILE)"
+	echo 'Alcatraz was removed from Xcode'\''s skipped plugins list. Next time you start Xcode select "Load Bundle" when prompted.'
+}
+rm "$TMP_FILE"
 
 mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
