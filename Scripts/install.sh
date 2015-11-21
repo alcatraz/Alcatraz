25c4b3d86f28b7f294e20b8f89c48b3860eb9f7e
Guillaume Algis <guillaume.algis@gmail.com>
2015-11-21 13:22:29 +0100

    Use mktemp instead of re-using the same tmp file
    + Fix coding style

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -7,14 +7,19 @@ PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins
 XCODE_VERSION="$(xcrun xcodebuild -version | head -n1 | awk '{ print $2 }')"
 PLIST_PLUGINS_KEY="DVTPlugInManagerNonApplePlugIns-Xcode-${XCODE_VERSION}"
 BUNDLE_ID="com.mneorr.Alcatraz"
-TMP_FILE="/tmp/${BUNDLE_ID}.xcode-defaults"
+TMP_FILE="$(mktemp -t ${BUNDLE_ID})"
 
 # Remove Alcatraz from Xcode's skipped plugins list if needed
 defaults read -app Xcode "$PLIST_PLUGINS_KEY" > "$TMP_FILE"
 /usr/libexec/PlistBuddy -c "delete skipped:$BUNDLE_ID" "$TMP_FILE" > /dev/null 2>&1 && {
-	pgrep Xcode > /dev/null && { echo 'An instance of Xcode is currently running. Please close Xcode before installing Alcatraz.'; exit 1; }
-	defaults write -app Xcode "$PLIST_PLUGINS_KEY" "$(< $TMP_FILE)"
-	echo 'Alcatraz was removed from Xcode'\''s skipped plugins list. Next time you start Xcode select "Load Bundle" when prompted.'
+    pgrep Xcode > /dev/null && {
+        echo 'An instance of Xcode is currently running.' \
+             'Please close Xcode before installing Alcatraz.'
+        exit 1
+    }
+    defaults write -app Xcode "$PLIST_PLUGINS_KEY" "$(cat "$TMP_FILE")"
+    echo 'Alcatraz was removed from Xcode'\''s skipped plugins list.' \
+         'Next time you start Xcode select "Load Bundle" when prompted.'
 }
 rm "$TMP_FILE"
 
@@ -22,5 +27,6 @@ mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
 # the 1 is not a typo!
-echo "Alcatraz successfully installed!!1!🍻   Please restart your Xcode ($XCODE_VERSION)."
+echo 'Alcatraz successfully installed!!1!🍻 ' \
+     "Please restart your Xcode ($XCODE_VERSION)."
 
