e44f91d3f7444e84dfb539d1a3ee3f0c5f4947a0
overtrue <anzhengchao@gmail.com>
2015-12-26 22:05:09 +0800

    Fix typo.
    maybe.

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -28,6 +28,6 @@ mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
 # the 1 is not a typo!
-echo 'Alcatraz successfully installed!!1!🍻 ' \
+echo 'Alcatraz successfully installed!!!!🍻 ' \
      "Please restart your Xcode ($XCODE_VERSION)."
 
