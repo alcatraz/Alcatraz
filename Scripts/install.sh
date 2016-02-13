2e5cc01db49988904d2ea943e0eb222ace5536ae
Guillaume Algis <guillaume.algis@gmail.com>
2016-02-13 21:23:41 +0100

    Silence 'The domain/default pair [...] does not exist' warnings

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euo pipefail
 
@@ -10,7 +10,8 @@ BUNDLE_ID="com.mneorr.Alcatraz"
 TMP_FILE="$(mktemp -t ${BUNDLE_ID})"
 
 # Remove Alcatraz from Xcode's skipped plugins list if needed
-defaults read com.apple.dt.Xcode "$PLIST_PLUGINS_KEY" > "$TMP_FILE" && {
+if defaults read com.apple.dt.Xcode "$PLIST_PLUGINS_KEY" > "$TMP_FILE"; then
+    # We read the prefs successfully, delete Alcatraz from the skipped list if needed
     /usr/libexec/PlistBuddy -c "delete skipped:$BUNDLE_ID" "$TMP_FILE" > /dev/null 2>&1 && {
 	pgrep Xcode > /dev/null && {
             echo 'An instance of Xcode is currently running.' \
@@ -21,9 +22,18 @@ defaults read com.apple.dt.Xcode "$PLIST_PLUGINS_KEY" > "$TMP_FILE" && {
 	echo 'Alcatraz was removed from Xcode'\''s skipped plugins list.' \
              'Next time you start Xcode select "Load Bundle" when prompted.'
     }
-}
+else
+    # Could not read the prefs. Filter known warnings, and exit for any other.
+    KNOWN_WARNING="The domain/default pair of \(.+, $PLIST_PLUGINS_KEY\) does not exist"
+
+    # tr: For some mysterious reason, some `defaults` errors are outputed on two lines.
+    # grep: -v returns 1 when output is empty (ie. we filtered the known warning)
+    # so we exit on 0, which means an unknown error occured.
+    tr -d '\n' < "$TMP_FILE" | egrep -v "$KNOWN_WARNING" && exit 1
+fi
 rm -f "$TMP_FILE"
 
+# Download and install Alcatraz
 mkdir -p "${PLUGINS_DIR}"
 curl -L $DOWNLOAD_URI | tar xvz -C "${PLUGINS_DIR}"
 
